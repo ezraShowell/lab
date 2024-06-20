72f3516d0d5c5035e857f40427c7d93e7146671d
ezraShowell <ezra111704@gmail.com>
2024-06-20 19:35:28 -0400

    changes for lab I hope this works :(

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Card from './Card';
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section 001</h2>
       <p>WVU ID: 800393532</p>
       <p>Hi I am Anthony</p>
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
